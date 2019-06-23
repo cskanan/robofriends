a12ac45f4e3f56c3414075d9601bc34c19724b26
Sureshkanan Chandrakanthan <cskanan@gmail.com>
2019-06-23 17:31:19 +0800

    Updates

---
+++ precache-manifest.db12f127c70b043d54a178c2a0a08c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5be0c2c27300b5c1066be385c533793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "36cdd8b21bae8912b45f",
+    "url": "/static/css/main.80c4a75e.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "36cdd8b21bae8912b45f",
+    "url": "/static/js/main.1a1ccf95.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
